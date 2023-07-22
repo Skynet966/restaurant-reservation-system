dd744a6fd85c4d644e3ddf45abb118927a4afefc
Satish Yadav <satishkumaryadav72@gmail.com>
2023-07-22 20:44:25 +0530

    Init application

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Hello!!, this is the start of a new era.</h1>
+      </section>
+    </main>
+  )
+}
